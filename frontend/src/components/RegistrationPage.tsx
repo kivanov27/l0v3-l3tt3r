425246224bf5c142386d7c5a69ff1f8446a88be3
fdd <kristiyan.ivanov.98@gmail.com>
2025-11-18 17:19:12 +0200

    nothing basically

--- frontend/src/components/RegistrationPage.tsx
+++ frontend/src/components/RegistrationPage.tsx
@@ -5,11 +5,12 @@ const RegistrationPage = () => {
     const [password, setPassword] = useState<string>('');
 
     return (
-        <div>
+        <div className="user-form-container">
             <label htmlFor="username">username:</label>
             <input id="username" type="text" onChange={({ target }) => setUsername(target.value)} />
             <label htmlFor="password">password:</label>
             <input id="password" type="password" onChange={({ target }) => setPassword(target.value)} />
+            <button>register</button>
         </div>
     );
 };
